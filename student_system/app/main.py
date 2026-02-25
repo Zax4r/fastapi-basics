9050f4e09817a9bc43f58a9e8763b61ddaada675
Zax4r <zaharbedarik@gmail.com>
2026-02-25 21:35:34 +0300

    added html response

--- student_system/app/main.py
+++ student_system/app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 import os
-from app.students.router import router_students
-from app.majors.router import router_majors
-
-
+from app.students.router import students_router
+from app.majors.router import majors_router
+from app.users.router import users_router
+from app.pages.router import pages_router
 
 
 app = FastAPI()
 
-app.include_router(router_students)
-app.include_router(router_majors)
-
+app.include_router(students_router)
+app.include_router(majors_router)
+app.include_router(users_router)
+app.include_router(pages_router)
