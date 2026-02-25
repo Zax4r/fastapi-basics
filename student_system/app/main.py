69e015af2acaaa4f4bc2cd5000572a44c0517d50
Zax4r <zaharbedarik@gmail.com>
2026-02-25 22:19:20 +0300

    router update

--- student_system/app/main.py
+++ student_system/app/main.py
@@ -4,9 +4,12 @@ from app.students.router import students_router
 from app.majors.router import majors_router
 from app.users.router import users_router
 from app.pages.router import pages_router
+from fastapi.staticfiles import StaticFiles
+
 
 
 app = FastAPI()
+app.mount('/static',StaticFiles(directory='app/static'),'static')
 
 app.include_router(students_router)
 app.include_router(majors_router)
