fd5b5394b4473ede7060ca3bcad9b6848f27894c
Zax4r <zaharbedarik@gmail.com>
2026-02-26 23:51:59 +0300

    tasks router included

--- to_do/app/main.py
+++ to_do/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.routers.users import router as user_router
+from app.routers.tasks import router as task_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(task_router)
+
 
 @app.get('/')
 async def root():
