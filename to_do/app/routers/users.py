4920ef4513f48d71b78f5f59487f2678893be9bf
Zax4r <zaharbedarik@gmail.com>
2026-02-26 11:02:50 +0300

    users endpoint

--- to_do/app/routers/users.py
+++ to_do/app/routers/users.py
@@ -1,8 +1,17 @@
-from app.schemas.user import SUserAdd
+from app.schemas.users import SUserAdd, SUserAnswer
 from fastapi import APIRouter
+from app.dependecies import DbDep
+from typing import List
+from app.services.users import UserService
+
 
 router = APIRouter(prefix='/users',tags=['Работа с пользователем'])
 
 @router.post('/add/')
 async def add_user(user: SUserAdd):
     return user
+
+@router.get('/', response_model=List[SUserAnswer])
+async def get_users(session: DbDep):
+    users = await UserService.get_all(session)
+    return users
--- to_do/app/schemas/users.py
+++ to_do/app/schemas/users.py
@@ -8,4 +8,6 @@ class SUserAdd(SUserBase):
     email: EmailStr = Field(...)
     password: str = Field(...,min_length=4)
 
-
+class SUserAnswer(SUserBase):
+    id: int = Field(...)
+    email: EmailStr = Field(...)
