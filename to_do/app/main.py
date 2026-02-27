d0d4fba6ed3eb64a90a07c365426cbdb87c5bc83
Zax4r <zaharbedarik@gmail.com>
2026-02-27 12:22:49 +0300

    added auth router

--- to_do/app/main.py
+++ to_do/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.routers.users import router as user_router
 from app.routers.tasks import router as task_router
+from app.routers.auth import router as auth_router
 
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(task_router)
+app.include_router(auth_router)
 
 
 @app.get('/')
