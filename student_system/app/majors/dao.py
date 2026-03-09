ae8eb1ec40be1353d1dbfc8cdd0fdf9a54693d76
Zax4r <zaharbedarik@gmail.com>
2026-03-09 23:28:07 +0300

    moved directories

--- student_system/app/majors/dao.py
+++
@@ -1,7 +0,0 @@
-from app.dao.base import BaseDAO
-from app.majors.models import Major
-
-
-class MajorDAO(BaseDAO):
-
-    model = Major
--- student_system/app/users/dao.py
+++
@@ -1,5 +0,0 @@
-from app.users.models import User
-from app.dao.base import BaseDAO
-
-class UserDAO(BaseDAO):
-    model = User
