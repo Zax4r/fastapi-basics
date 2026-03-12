127d7e8c0c1dc863735735d3f52b34c6b3cef781
Zax4r <zaharbedarik@gmail.com>
2026-03-12 10:41:24 +0300

    better responses for tests

--- to_do/backend/app/routers/tasks.py
+++ to_do/backend/app/routers/tasks.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from app.services.tasks import TaskService
 from app.schemas.tasks import STaskAdd, STaskShow, STaskUpd
 from app.models.dependecies import DbDep
@@ -8,31 +8,40 @@ from typing import List
 
 router = APIRouter(prefix='/tasks',tags=['Работа с задачами'])
 
-@router.post('/add/')
+@router.post('/add/', response_model= STaskAdd)
 async def add_task(add_task: STaskAdd, session: DbDep, current_user: CUDep):
     new_task = add_task.model_dump()
     new_task['user_id'] = current_user.id
     check = await TaskService.add_one(session,**new_task)
-    if check:
-        return {'message':f'Задача {new_task['task_name']} для пользователя {new_task['user_id']} добавлена'}
-    return {'message':f'Ошибка при добавлении {new_task['task_name']} для пользователя {new_task['user_id']}'}
+    if not check:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Error with adding task"
+        )
+    return check
 
-@router.put('/update/{task_id}')
+@router.put('/update/{task_id}', response_model= STaskUpd)
 async def update_task(task_id: int, task_upd: STaskUpd, session: DbDep):
     new_task = task_upd.model_dump()
     check = await TaskService.update_one(session, task_id, **new_task)
-    if check:
-        return {'message':f'Задача {new_task['task_name']} обновлена'}
-    return {'message':f'Ошибка при обновлении {new_task['task_name']}'}
+    if not check:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Error with updating task"
+        )
+    return task_upd
 
 
 
 @router.delete('/delete/{task_id}')
 async def delete_task(task_id: int, session: DbDep, current_user: CUDep):
     check = await TaskService.delete_task(session, task_id, current_user.id)
-    if check:
-        return {'message':f'Задача успешно удалена'}
-    return {'message':f'Задача не удалена удалена'}
+    if not check:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Error with deleting task"
+        )
+    return {'message': f'Task deleted'}
 
 
 @router.get('/', response_model=List[STaskShow])
